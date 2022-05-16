791010ccde5797b1b41f9590f469b9a154541a4f
gueyenono <nonoghislain@gmail.com>
2022-05-16 08:57:11 -0500

    polished the function a bit

--- R/full_dlm_specification.R
+++ R/full_dlm_specification.R
@@ -1,8 +1,3 @@
-# source(here::here("R/visualizations.R"))
-# source(here::here("R/diagnostic_tests.R"))
-# source(here::here("R/log-lik_aic.R"))
-
-
 #' Dynamic Linear Model Estimation
 #'
 #' @param series Numeric vector of the data
@@ -18,42 +13,45 @@
 #' Soon!
 full_dlm_modeling <- function(
     series,
-    state_components = "level",
+    state_components = NULL,
     deterministic_components = NULL,
     seasonal_frequency = NULL,
     reg_data = NULL
 ){
 
-  # Initial error handling stuff
+  # ERROR HANDLING HERE!!!
 
-  stopifnot("No frequency was defined for the seasonal component of the model." = ("seasonal" %in% state_components & is.numeric(seasonal_frequency)) | (!"seasonal" %in% state_components & is.null(seasonal_frequency)))
+  # stopifnot("The 'state_components' argument suggests that the model contains at least one explanatory variable, but no data was provided in the 'reg_data' argument." = "regressor" %in% state_components & is.null(reg_data))
+  #
+  # stopifnot("The 'reg_data' argument must be of one of the following classes: 'numeric', 'matrix' or 'data.frame'." = ("regressor" %in% state_components & class(reg_data)[1] %in% c("matrix", "numeric", "double", "data.frame")) | (!"regressor" %in% state_components & is.null(reg_data)))
+  #
+  # stopifnot("The state_components argument must contain any combination of 'level' (mandatory), 'slope', 'seasonal' and 'regressor'." =  all(state_components %in% c("level", "slope", "seasonal", "regressor")))
 
-  stopifnot("The regression data needs to be provided to the 'reg_data' argument and it must be a numeric vector, a matrix or a data frame." = ("regressor" %in% state_components & class(reg_data)[1] %in% c("matrix", "numeric", "double", "data.frame")) | (!"regressor" %in% state_components & is.null(reg_data)))
+  # Error: If a deterministic component is not specified as a state component
+  # Error: "regressor" instead of "reg1", "reg2", ... in deterministic_components
 
-  stopifnot("The state_components argument must contain any combination of 'level' (mandatory), 'slope', 'seasonal' and 'regressor'." =  all(state_components %in% c("level", "slope", "seasonal", "regressor")))
+  # browser()
 
-  browser()
 
-  # Initialize key variables for the model
+  # Order of the polynomial model
 
   order <- dplyr::case_when(
     !"slope" %in% state_components ~ 1,
     "slope" %in% state_components ~ 2
   )
 
-  if(!is.null(reg_data) & is.atomic(reg_data)) reg_data <- as.matrix(reg_data)
+  # Transform regression data from atomic vector to matrix
 
-  # Number of state variables
+  if("regressor" %in% state_components & is.atomic(reg_data)) reg_data <- as.matrix(reg_data)
+
+  # Calculate number of state variables
 
   n_seasonality <- ifelse("seasonal" %in% state_components, seasonal_frequency - 1, 0)
   n_regressors <- ifelse("regressor" %in% state_components, ncol(reg_data), 0)
-  non_seas_and_reg_state_comps <- state_components[!state_components %in% c("seasonal", "regressor")]
-  n_state_vars <- length(non_seas_and_reg_state_comps) + n_seasonality + n_regressors
+  level_and_or_slope_comps <- grep(pattern = "level|slope", x = state_components, value = TRUE)
+  n_state_vars <- length(level_and_or_slope_comps) + n_seasonality + n_regressors
 
-  # Names and number of hyperparameters
-
-  non_reg_state_comps <- state_components[!state_components %in% "regressor"]
-  n_deter_reg <- sum(grepl(pattern = "reg\\d+", x = deterministic_components, ignore.case = TRUE)) # Number of deterministic regressors
+  # Stochastic hyperparameters
 
   state_components2 <- c(
     setdiff(state_components, "regressor"),
@@ -67,108 +65,91 @@ full_dlm_modeling <- function(
     }
   )
 
-  hyper_params <- setdiff(state_components2, deterministic_components)
-  n_hyper_params <- 1 + length(hyper_params)
+  stoch_state_comps <- setdiff(state_components2, deterministic_components)
+  n_hyper_params <- 1 + length(stoch_state_comps)
 
-  # n_hyper_params <- length(non_reg_state_comps) + n_regressors - length(deterministic_components)
+  # Initial values of the stochastic hyperparameters
 
-  # Inital values for the hyperparameters
-  hyper_params_initial <- c(
+  parm <- hyper_params_initial <- c(
     var(series),
     rep(0.001, n_hyper_params-1)
   )
-
-  # Column names of the data frame of estimated values (at the very bottom!)
-
-  col_names <- lapply(state_components, function(s){
-    out <- NULL
-    if(s == "level"){
-      out <- "level"
-    }
-    if(s == "slope"){
-      out <- "slope"
-    }
-    if(s == "seasonal"){
-      out <- paste0("seas", seq_len(seasonal_frequency-1))
-    }
-    if(s == "regressor"){
-      l <- ifelse(
-        is.atomic(reg_data),
-        1,
-        ncol(reg_data)
-      )
-      out <- paste0("reg", seq_len(l))
-    }
-    out
-  }) %>%
-    Reduce(f = c, x = .)
-
 
   # Function for building the model
 
   func_dlm_mod <- function(parm){
 
     mod_list <- list()
-    non_seas_and_reg_deter_comps <- deterministic_components[!grepl(pattern = "seasonal|reg\\d+", x = deterministic_components)]
 
+    # Level/slope component
     mod_list$poly <- dlm::dlmModPoly(
       order = order,
       dV = exp(parm[1]),
       dW = {
-        out <- purrr::imap_chr(non_seas_and_reg_state_comps, function(state, i){
-          ifelse(state %in% non_seas_and_reg_deter_comps, "0", glue::glue("exp(parm[{i+1}])"))
-        }) %>%
-          str2expression()
-        purrr::map_dbl(out, ~ eval(.x))
+        dW_poly <- glue::glue("exp(parm[{1+seq_len(order)}])")
+        index_deter_poly <- purrr::imap_dbl(level_and_or_slope_comps, function(comp, i){
+          if(comp %in% deterministic_components){
+            return(i)
+          } else {
+            return(NA)
+          }
+        })
+        dW_poly[index_deter_poly] <- "0"
+        dW_poly |>
+          str2expression() |>
+          purrr::map_dbl(~ eval(.x))
       }
     )
 
+    # Seasonal component
     if("seasonal" %in% state_components){
-
-      seas_comp_i <- which(state_components == "seasonal")
-
-      if("seasonal" %in% deterministic_components){
-        dW <- rep(0, seasonal_frequency - 1)
-      } else {
-        dW <- c(
-          eval(str2expression(glue::glue("exp(parm[{seas_comp_i+1}])"))),
-          rep(0, seasonal_frequency - 2)
-        )
-      }
 
       mod_list$seas <- dlm::dlmModSeas(
         frequency = seasonal_frequency,
         dV = 0,
-        dW = dW
+        dW = {
+          dW_seas <- rep(0, times = seasonal_frequency - 1)
+          seas_comp_i <- which(stoch_state_comps == "seasonal")
+          if(!"seasonal" %in% deterministic_components) dW_seas[1] <- eval(str2expression(glue::glue("exp(parm[{1 + seas_comp_i}])")))
+          dW_seas
+        }
       )
+
     }
 
-
-
+    # Explanatory variable(s) components
     if("regressor" %in% state_components){
-
-      reg_comps <- paste0("reg", seq_len(ncol(reg_data)))
-      reg_deter_comps <- grep(pattern = "^reg\\d+$", x = deterministic_components, value = TRUE)
-
-      out <- purrr::imap_chr(reg_comps, function(reg_comp, i){
-        ifelse(reg_comp %in% reg_deter_comps, "0", glue::glue("exp(parm[{i}])"))
-      }) %>%
-        str2expression()
-
-      dW <- purrr::map_dbl(out, ~ eval(.x))
 
       mod_list$reg <- dlm::dlmModReg(
         X = reg_data,
         dV = 0,
-        dW = dW,
+        dW = {
+          dW_reg <- rep("0", n_regressors)
+          reg_comps <- paste0("reg", seq_len(n_regressors))
+          n_level_slope_stoch_comps <- sum(grepl(pattern = "level|slope", x = stoch_state_comps))
+          n_stoch_seas <- ifelse("seasonal" %in% deterministic_components, 1, 0)
+          reg_stoch_comps <- grep(pattern = "^reg\\d+$", x = stoch_state_comps, value = TRUE)
+
+          if(length(reg_stoch_comps) != 0){
+            reg_stoch_comp_i <- which(reg_comps %in% stoch_state_comps)
+            reg_stoch_comp_i2 <- which(reg_stoch_comps %in% stoch_state_comps) + n_level_slope_stoch_comps + n_stoch_seas + 1
+            dW_reg[reg_stoch_comp_i] <- glue::glue("exp(parm[{reg_stoch_comp_i2}])")
+          }
+
+          dW_reg |>
+            str2expression() |>
+            purrr::map_dbl(~ eval(.x))
+        },
         addInt = FALSE
       )
     }
 
     Reduce("+", mod_list)
+
   }
 
   # Estimation of the model's hyperparameters with Maximul Likelihood Estimation (MLE)
+
   hyper_parms_mle_est <- dlm::dlmMLE(
     y = series,
     parm = log(hyper_params_initial),
@@ -200,15 +181,39 @@ full_dlm_modeling <- function(
 
   # Data frame of all smooth estimates
 
-  smoothed_estimates <- tibble::as_tibble(dlm_smoothed$s, .name_repair = "unique") %>%
-    setNames(Reduce(f = c, x = col_names)) %>%
+  col_names0 <- lapply(state_components, function(s){
+    out <- NULL
+    if(s == "level"){
+      out <- "level"
+    }
+    if(s == "slope"){
+      out <- "slope"
+    }
+    if(s == "seasonal"){
+      out <- paste0("seas", seq_len(seasonal_frequency-1))
+    }
+    if(s == "regressor"){
+      l <- ifelse(
+        is.atomic(reg_data),
+        1,
+        ncol(reg_data)
+      )
+      out <- paste0("reg", seq_len(l))
+    }
+    out
+  })
+
+  col_names <- Reduce(f = c, x = col_names0)
+
+  smoothed_estimates <- tibble::as_tibble(dlm_smoothed$s, .name_repair = "unique") |>
+    setNames(Reduce(f = c, x = col_names)) |>
     dplyr::mutate(
       t = dplyr::row_number() - 1,
       series = c(NA, dlm_filtered$y)
-    ) %>%
+    ) |>
     dplyr::relocate(t, series, 1)
 
-  smoothed_estimates$level_plus_state <- purrr::map_dfc(state_components, function(comp){
+  level_plus_state <- purrr::map_dfc(state_components, function(comp){
     if(comp == "level"){
       out <- smoothed_estimates[["level"]][-1]
     }
@@ -225,9 +230,10 @@ full_dlm_modeling <- function(
       out <- smoothed_estimates[["seas1"]][-1]
     }
     out
-  }) %>%
-    rowSums() %>%
-    c(NA, .)
+  }) |>
+    rowSums()
+
+  smoothed_estimates$level_plus_state <- c(NA, level_plus_state)
 
   smoothed_estimates$residuals <- c(NA, series) - smoothed_estimates$level_plus_state
   smoothed_estimates$residuals_raw <- c(NA, dlm:::residuals.dlmFiltered(object = dlm_filtered, type = "raw", sd = FALSE))
@@ -254,5 +260,6 @@ full_dlm_modeling <- function(
   class(out) <- "dlm_model"
 
   out
+
 
 }
--- R/test_functions.R
+++ R/test_functions.R
@@ -1,191 +1,191 @@
-# Source function ----
-
-source(file = here::here("R/full_dlm_specification2.R"))
-source(file = here::here("R/log_likelihood-and-aic.R"))
-
-# Get the data ----
-
-# > UK KSI
-uk_ksi_raw <- readr::read_lines(file = here::here("data-raw/UKdriversKSI.txt"), skip = 1)
-
-uk_ksi <- tibble::tibble(
-  date = seq(from = as.Date("1969-01-01"), by = "month", length.out = length(uk_ksi_raw)),
-  value = as.numeric(uk_ksi_raw),
-  log_value = log(value)
-)
-
-# > UK gas price
-gas_price_data <- readr::read_lines(file = here::here("data-raw/logUKpetrolprice.txt"), skip = 1) |>
-  as.numeric()
-
-# Seat belt law dummy
-seat_belt_dummy <- rep(0, nrow(uk_ksi))
-seat_belt_dummy[170:length(seat_belt_dummy)] <- 1
-
-# Chapter 2: The local level model ----
-
-# 2.1 - Deterministic level
-
-(llm1 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = "level",
-  deterministic_components = "level"
-))
-
-# 2.2 - Stochastic level
-
-(llm2 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = "level",
-  deterministic_components = NULL
-))
-
-# 2.3 - The local level model and Norwegian fatalities
-
-# ...
-
-# Chapter 3: The local linear trend model ----
-
-# 3.1 Deterministic level and slope
-
-(lltm1 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "slope"),
-  deterministic_components = c("level", "slope")
-))
-
-# 3.2 Stochastic level and slope
-
-(lltm2 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "slope"),
-  deterministic_components = NULL
-))
-
-# 3.3 Stochastic level and deterministic slope
-
-(lltm3 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "slope"),
-  deterministic_components = "slope"
-))
-
-# 3.4 The local linear trend model and Finnish fatalities
-
-# ...
-
-# Chapter 4: The local level model with seasonal ----
-
-# 4.1 Deterministic level and seasonal
-
-(llms1 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "seasonal"),
-  deterministic_components = c("level", "seasonal"),
-  seasonal_frequency = 12
-))
-
-# 4.2 Stochastic level and seasonal
-
-(llms2 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "seasonal"),
-  deterministic_components = NULL,
-  seasonal_frequency = 12
-))
-
-# 4.3 Stochastic level and deterministic seasonal
-
-(llms3 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "seasonal"),
-  deterministic_components = "seasonal",
-  seasonal_frequency = 12
-))
-
-# 4.4 The local level and seasonal model and UK inflation
-
-# ...
-
-# Chapter 5 - The local level model with explanatory variable
-
-# 5.1a Deterministic level and explanatory variable
-
-(llmev1 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "regressor"),
-  deterministic_components = c("level", "reg1"),
-  reg_data = as.numeric(seq_len(nrow(uk_ksi)))
-))
-
-# 5.1b Deterministic level and deterministic explanatory variable
-
-(llmev1b <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "regressor"),
-  deterministic_components = c("level", "reg1"),
-  reg_data = gas_price_data
-))
-
-# 5.2 Stochastic level and deterministic explanatory variable
-
-(llmev2 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "regressor"),
-  deterministic_components = "reg1",
-  reg_data = gas_price_data
-))
-
-
-# Chapter 6 - The local level model with intervention variable
-
-# 6.1 Deterministic level and deterministic intervention variable
-
-(llmiv1 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "regressor"),
-  deterministic_components = c("level", "reg1"),
-  reg_data = seat_belt_dummy
-))
-
-# 6.2 Stochastic level and deterministic intervention variable
-
-(llmiv2 <- full_dlm_modeling(
-  series = uk_ksi$log_value,
-  state_components = c("level", "regressor"),
-  deterministic_components = "reg1",
-  reg_data = seat_belt_dummy
-))
-
-# initial_values <- c(
-#   var(uk_ksi$log_value),
-#   rep(0.001, 2)
+# # Source function ----
+#
+# source(file = here::here("R/full_dlm_specification.R"))
+# source(file = here::here("R/log_likelihood-and-aic.R"))
+#
+# # Get the data ----
+#
+# # > UK KSI
+# uk_ksi_raw <- readr::read_lines(file = here::here("data-raw/UKdriversKSI.txt"), skip = 1)
+#
+# uk_ksi <- tibble::tibble(
+#   date = seq(from = as.Date("1969-01-01"), by = "month", length.out = length(uk_ksi_raw)),
+#   value = as.numeric(uk_ksi_raw),
+#   log_value = log(value)
 # )
 #
-# build_func <- function(parm){
-#   mod1 <- dlm::dlmModPoly(
-#     order = 1,
-#     dV = exp(parm[1]),
-#     dW = exp(parm[2])
-#   )
-#   mod2 <- dlm::dlmModReg(
-#     X = gas_price_data,
-#     dV = 0,
-#     dW = exp(parm[3]),
-#     addInt = FALSE
-#   )
-#   mod1 + mod2
-# }
+# # > UK gas price
+# gas_price_data <- readr::read_lines(file = here::here("data-raw/logUKpetrolprice.txt"), skip = 1) |>
+#   as.numeric()
 #
-# mle_est <- dlm::dlmMLE(
-#   y = uk_ksi$log_value,
-#   parm = log(initial_values),
-#   build = build_func
-# )
+# # Seat belt law dummy
+# seat_belt_dummy <- rep(0, nrow(uk_ksi))
+# seat_belt_dummy[170:length(seat_belt_dummy)] <- 1
 #
-# mod <- build_func(parm = mle_est$par)
+# # Chapter 2: The local level model ----
 #
-# filt <- dlm::dlmFilter(y = uk_ksi$log_value, mod = mod)
-# smot <- dlm::dlmSmooth(y = filt, mod = mod)
+# # 2.1 - Deterministic level
 #
-# smot$s[2,1]
+# (llm1 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = "level",
+#   deterministic_components = "level"
+# ))
+#
+# # 2.2 - Stochastic level
+#
+# (llm2 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = "level",
+#   deterministic_components = NULL
+# ))
+#
+# # 2.3 - The local level model and Norwegian fatalities
+#
+# # ...
+#
+# # Chapter 3: The local linear trend model ----
+#
+# # 3.1 Deterministic level and slope
+#
+# (lltm1 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "slope"),
+#   deterministic_components = c("level", "slope")
+# ))
+#
+# # 3.2 Stochastic level and slope
+#
+# (lltm2 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "slope"),
+#   deterministic_components = NULL
+# ))
+#
+# # 3.3 Stochastic level and deterministic slope
+#
+# (lltm3 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "slope"),
+#   deterministic_components = "slope"
+# ))
+#
+# # 3.4 The local linear trend model and Finnish fatalities
+#
+# # ...
+#
+# # Chapter 4: The local level model with seasonal ----
+#
+# # 4.1 Deterministic level and seasonal
+#
+# (llms1 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "seasonal"),
+#   deterministic_components = c("level", "seasonal"),
+#   seasonal_frequency = 12
+# ))
+#
+# # 4.2 Stochastic level and seasonal
+#
+# (llms2 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "seasonal"),
+#   deterministic_components = NULL,
+#   seasonal_frequency = 12
+# ))
+#
+# # 4.3 Stochastic level and deterministic seasonal
+#
+# (llms3 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "seasonal"),
+#   deterministic_components = "seasonal",
+#   seasonal_frequency = 12
+# ))
+#
+# # 4.4 The local level and seasonal model and UK inflation
+#
+# # ...
+#
+# # Chapter 5 - The local level model with explanatory variable
+#
+# # 5.1a Deterministic level and explanatory variable
+#
+# (llmev1 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "regressor"),
+#   deterministic_components = c("level", "reg1"),
+#   reg_data = as.numeric(seq_len(nrow(uk_ksi)))
+# ))
+#
+# # 5.1b Deterministic level and deterministic explanatory variable
+#
+# (llmev1b <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "regressor"),
+#   deterministic_components = c("level", "reg1"),
+#   reg_data = gas_price_data
+# ))
+#
+# # 5.2 Stochastic level and deterministic explanatory variable
+#
+# (llmev2 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "regressor"),
+#   deterministic_components = "reg1",
+#   reg_data = gas_price_data
+# ))
+#
+#
+# # Chapter 6 - The local level model with intervention variable
+#
+# # 6.1 Deterministic level and deterministic intervention variable
+#
+# (llmiv1 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "regressor"),
+#   deterministic_components = c("level", "reg1"),
+#   reg_data = seat_belt_dummy
+# ))
+#
+# # 6.2 Stochastic level and deterministic intervention variable
+#
+# (llmiv2 <- full_dlm_modeling(
+#   series = uk_ksi$log_value,
+#   state_components = c("level", "regressor"),
+#   deterministic_components = "reg1",
+#   reg_data = seat_belt_dummy
+# ))
+#
+# # initial_values <- c(
+# #   var(uk_ksi$log_value),
+# #   rep(0.001, 2)
+# # )
+# #
+# # build_func <- function(parm){
+# #   mod1 <- dlm::dlmModPoly(
+# #     order = 1,
+# #     dV = exp(parm[1]),
+# #     dW = exp(parm[2])
+# #   )
+# #   mod2 <- dlm::dlmModReg(
+# #     X = gas_price_data,
+# #     dV = 0,
+# #     dW = exp(parm[3]),
+# #     addInt = FALSE
+# #   )
+# #   mod1 + mod2
+# # }
+# #
+# # mle_est <- dlm::dlmMLE(
+# #   y = uk_ksi$log_value,
+# #   parm = log(initial_values),
+# #   build = build_func
+# # )
+# #
+# # mod <- build_func(parm = mle_est$par)
+# #
+# # filt <- dlm::dlmFilter(y = uk_ksi$log_value, mod = mod)
+# # smot <- dlm::dlmSmooth(y = filt, mod = mod)
+# #
+# # smot$s[2,1]
